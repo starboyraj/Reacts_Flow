6e940a0636e2596a0c8e0e9806833f550b1d187c
Raj Singh <starboyraj88828@gmail.com>
2026-01-14 23:49:31 +0530

    add useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Card from './components/Card';
+import { useState } from "react";
+
+
 
 function App() {
+  const[count, setCount] = useState(0);
   return (
     <>
+    
+
+    <h1> Count : {count} </h1>
+    <button onClick={() => setCount(count+1)}>Add </button>
       <Navbar />
            <Card title="React" desc="Learning React step by step" />
            <Card title="JavaScript" desc="Core language for frontend" />
       <Footer />
+
+
     </>
   );
 }
